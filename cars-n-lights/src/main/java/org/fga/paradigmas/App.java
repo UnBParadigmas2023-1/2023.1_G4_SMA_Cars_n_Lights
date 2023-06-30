187fbca881c5459e07fe6e514fd29c204e970956
douglasmonteles <silvadouglas328@gmail.com>
2023-06-29 21:47:18 -0300

    Converting App into a jade agent

--- cars-n-lights/src/main/java/org/fga/paradigmas/App.java
+++ cars-n-lights/src/main/java/org/fga/paradigmas/App.java
@@ -1,10 +1,12 @@
 package org.fga.paradigmas;
 
+import jade.core.Agent;
+
 /**
  * Hello world!
  *
  */
-public class App {
+public class App extends Agent {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
