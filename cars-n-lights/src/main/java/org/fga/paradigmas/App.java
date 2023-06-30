8b8406bef36583fb5325ac2c55bb5318ea1648b9
douglasmonteles <silvadouglas328@gmail.com>
2023-06-29 21:20:41 -0300

    Init project

---
+++ cars-n-lights/src/main/java/org/fga/paradigmas/App.java
@@ -0,0 +1,13 @@
+package org.fga.paradigmas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
