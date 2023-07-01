ee7c9ccc362636ad8ce40e806313e10a6fd843ba
douglasmonteles <silvadouglas328@gmail.com>
2023-07-01 20:49:30 -0300

    Adding swing interface
    Co-authored-by: HacKairos <joao.brasilpv@gmail.com>

--- cars-n-lights/src/main/java/org/fga/paradigmas/screens/MainScreen.java
+++ cars-n-lights/src/main/java/org/fga/paradigmas/screens/MainScreen.java
@@ -1,2 +1,27 @@
-package org.fga.paradigmas.screens;public class MainScreen {
+package org.fga.paradigmas.screens;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreen extends JFrame {
+
+    public MainScreen(String title, Integer width, Integer height) {
+        setTitle(title);
+        setSize(width, height);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+
+        this.initMainScreen();
+    }
+
+    private void initMainScreen() {
+        SwingUtilities.invokeLater(() -> {
+            setVisible(true);
+
+            // Componente que gera o grafo
+            getContentPane().add(new Label("Ola, mundo"));
+        });
+    }
+
 }
